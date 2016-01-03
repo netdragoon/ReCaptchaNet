d31639edba406cb7b1236d803bef7780294dac56
NetDragoon <netdragoon@outlook.com>
2016-01-03 21:23:53 -0200

    test

---
+++ Canducci.ReCAPTCHA.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace Canducci.ReCAPTCHA.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            ReCaptchaResponse resp = new ReCaptchaResponse("0000", ":::1");
+            Assert.False(resp.Success);
+        }
+    }
+}
